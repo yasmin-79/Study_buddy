595a8a4cb22d82475f04236347cceecc06505168
yasmin-79 <yasminshaik4641@gmail.com>
2025-10-05 09:54:12 +0530

    Use live backend URL for deployment

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,7 @@
 // src/config.js
 
 // Replace with your live backend URL when deploying
-const BACKEND_URL = "http://localhost:5000"; // for local testing
+const BACKEND_URL ="https://study-buddy-4926.onrender.com"; // for local testing
 // const BACKEND_URL = "https://your-live-backend.com"; // for live demo
 
 export default BACKEND_URL;
